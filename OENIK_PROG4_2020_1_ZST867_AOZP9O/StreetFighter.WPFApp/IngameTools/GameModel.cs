6a426e41429a718097b57d444583446be4319929
tumpekdavid <tumpekdavid0402@gmail.com>
2020-04-11 19:16:27 +0200

    added GameModel class

---
+++ OENIK_PROG4_2020_1_ZST867_AOZP9O/StreetFighter.WPFApp/IngameTools/GameModel.cs
@@ -0,0 +1,21 @@
+﻿using StreetFighter.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.WPFApp.IngameTools
+{
+    class GameModel
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+
+        public GameModel()
+        {
+            Player1 = new Player();
+            Player2 = new Player();
+        }
+    }
+}
