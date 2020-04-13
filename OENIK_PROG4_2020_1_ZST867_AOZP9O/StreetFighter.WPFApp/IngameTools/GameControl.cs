e68fe2d166f49969349868844282af2ba005044e
tumpekdavid <tumpekdavid0402@gmail.com>
2020-04-13 22:39:04 +0200

    added GameControl class skeleton (previous commit's comment was a fail)

---
+++ OENIK_PROG4_2020_1_ZST867_AOZP9O/StreetFighter.WPFApp/IngameTools/GameControl.cs
@@ -0,0 +1,47 @@
+﻿using StreetFighter.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace StreetFighter.WPFApp.IngameTools
+{
+    public class GameControl : FrameworkElement
+    {
+        GameLogic logic;
+        GameRenderer renderer;
+        GameModel model;
+
+        public GameControl()
+        {
+            Loaded += GameControl_Loaded;
+        }
+
+        private void GameControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.logic = new GameLogic();
+            this.model = new GameModel();
+            this.renderer = new GameRenderer(this.model);
+            
+
+            Window win = Window.GetWindow(this);
+            if (win != null)
+            {
+               //TODO: ticks to start...
+            }
+
+            InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            if (this.renderer != null)
+            {
+                this.renderer.DrawThings(drawingContext);
+            }
+        }
+    }
+}
