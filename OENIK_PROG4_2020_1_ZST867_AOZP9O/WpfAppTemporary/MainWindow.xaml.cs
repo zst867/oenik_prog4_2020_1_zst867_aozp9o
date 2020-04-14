c5d293cbcdd33043c609097cc685cb332b135663
zst867 <hanulabenedek@gmail.com>
2020-04-14 09:32:41 +0200

    main menu can be scaled

--- OENIK_PROG4_2020_1_ZST867_AOZP9O/WpfAppTemporary/MainWindow.xaml.cs
+++ OENIK_PROG4_2020_1_ZST867_AOZP9O/WpfAppTemporary/MainWindow.xaml.cs
@@ -19,14 +19,71 @@ namespace WpfAppTemporary
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class mainWindow : Window
     {
         private readonly MainMenuViewModel vm;
 
-        public MainWindow()
+        public mainWindow()
         {
             InitializeComponent();
             vm = FindResource("my_viewmodel") as MainMenuViewModel;
+        }
+        public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register("ScaleValue", typeof(double), typeof(mainWindow), new UIPropertyMetadata(1.0, new PropertyChangedCallback(OnScaleValueChanged), new CoerceValueCallback(OnCoerceScaleValue)));
+
+        private static object OnCoerceScaleValue(DependencyObject o, object value)
+        {
+            mainWindow mainWindow = o as mainWindow;
+            if (mainWindow != null)
+                return mainWindow.OnCoerceScaleValue((double)value);
+            else
+                return value;
+        }
+
+        private static void OnScaleValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            mainWindow mainWindow = o as mainWindow;
+            if (mainWindow != null)
+                mainWindow.OnScaleValueChanged((double)e.OldValue, (double)e.NewValue);
+        }
+
+        protected virtual double OnCoerceScaleValue(double value)
+        {
+            if (double.IsNaN(value))
+                return 1.0f;
+
+            value = Math.Max(0.1, value);
+            return value;
+        }
+
+        protected virtual void OnScaleValueChanged(double oldValue, double newValue)
+        {
+
+        }
+
+        public double ScaleValue
+        {
+            get
+            {
+                return (double)GetValue(ScaleValueProperty);
+            }
+            set
+            {
+                SetValue(ScaleValueProperty, value);
+            }
+        }
+
+
+        private void MainGrid_SizeChanged(object sender, EventArgs e)
+        {
+            CalculateScale();
+        }
+
+        private void CalculateScale()
+        {
+            double yScale = ActualHeight / 600f;
+            double xScale = ActualWidth / 400f;
+            double value = Math.Min(xScale, yScale);
+            ScaleValue = (double)OnCoerceScaleValue(myMainWindow, value);
         }
     }
 }
